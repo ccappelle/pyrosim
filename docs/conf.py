ac11f1c240d2c442d660247bfd9eea22ec430b69
Collin Cappelle <collin.cappelle@gmail.com>
2017-06-09 13:22:08 -0400

    Documentation commit

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,8 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath('../pyrosim/.'))
 
 # -- General configuration ------------------------------------------------
@@ -124,7 +126,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
