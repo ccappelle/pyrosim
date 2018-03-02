d0d0c9155eabfca1b2b123e673b24d5444bce113
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2018-03-01 20:36:06 -0500

    Angular component for the proximity sensor + math
    Fully funcitonal proximity sensor. Had to add a lot of auxiliary
    math to implement it, although ended up using only a small subset
    of it in the end. The math is in myODEMath.h. Matrix code is
    mostly adapted from nonexposed ODE functions.
    
    	new file:   myODEMath.h
    	modified:   sensor/proximity.cpp

--- pyrosim/simulator/sensor/proximity.cpp
+++ pyrosim/simulator/sensor/proximity.cpp
@@ -3,14 +3,19 @@
 
 #include <iostream>
 #include <drawstuff/drawstuff.h>
+#include <math.h>
 #include "proximity.h"
 #include "object.h"
 #include "../geomData.h"
+#include "../myODEMath.h"
 
 #ifdef dDOUBLE
 #define dsDrawLine dsDrawLineD
 #define dsDrawSphere dsDrawSphereD
 #endif
+
+#define COORDINATE_SINGULARITY_CUTOFF 0.005
+#define ANGULAR_SINGULARITY_CUTOFF 0.005
 
 PROXIMITY_SENSOR::PROXIMITY_SENSOR(dSpaceID space, OBJECT* myObj, int myID, int evalPeriod) : SENSOR(myID, evalPeriod, 6),
                                                                                               basisObject(myObj),
@@ -24,7 +29,7 @@ PROXIMITY_SENSOR::PROXIMITY_SENSOR(dSpaceID space, OBJECT* myObj, int myID, int 
 	cr = values[3]; cg = values[4]; cb = values[5];
 
 	for(int tt=0; tt<evalPeriod; tt++) {
-		r[tt] = 0.0; t[tt] = 0.0; p[tt] = 0.0;
+		r[tt] = 0.0; t[tt] = 0.5; p[tt] = 0.5;
 	}
 
 	sensorVolume = dCreateSphere(space, sensorRadius);
@@ -63,9 +68,40 @@ void PROXIMITY_SENSOR::Set(double depth, dVector3 contactPoint, OBJECT* objectTh
 	if ( depth/sensorRadius < r[tt] )
 		return; // The proximity sensor stops when it hits its first object.
 
-	r[tt] = depth/sensorRadius;
+	r[tt] = depth/(sensorRadius*2.);
 
-	cpx = contactPoint[0]; cpy = contactPoint[1]; cpz = contactPoint[2];
+	// Computing the direction towards the contact point in the global coordinates
+	const dReal* cen = dGeomGetPosition(sensorVolume);
+	dVector3 contactDir;
+	myDVector3Subtract(contactPoint, cen, contactDir);
+	myDVector3Normalize(contactDir);
+
+	// Computing the transformation matrix TO the body's coordinate system
+	const dReal* sensorRotation = dGeomGetRotation(sensorVolume);
+	dReal invertedSensorRotation[12];
+	myDMatrix3Copy(sensorRotation, invertedSensorRotation);
+	myDMatrix3Transpose(invertedSensorRotation); // R^{-1} = R^T! https://en.wikipedia.org/wiki/Rotation_matrix#Properties_of_a_rotation_matrix
+
+	// Computing the direction towards the contact point in the coordinate system attached to the body...
+	dVector3 localContactDir;
+	myDMultiplyMat3Vec3(invertedSensorRotation, contactDir, localContactDir);
+	// ...and translating it into spherical coordinates
+	dReal theta = asin(localContactDir[2]);
+	dReal phi;
+	if(theta < ANGULAR_SINGULARITY_CUTOFF || M_PI-theta < ANGULAR_SINGULARITY_CUTOFF)
+		phi = 0.;
+	else if(abs(localContactDir[0]) < COORDINATE_SINGULARITY_CUTOFF)
+		phi = localContactDir[1] > 0. ? M_PI_2 : -M_PI_2;
+	else
+		phi = atan(localContactDir[1]/localContactDir[0]);
+
+	t[tt] = theta / M_PI;
+	p[tt] = (phi + M_PI) / (2. * M_PI);
+
+//	myPrintDVector3(std::cout, localContactDir);
+//	std::cout << '\n';
+
+	cpx = contactPoint[0]; cpy = contactPoint[1]; cpz = contactPoint[2]; // stored here for subsequent drawing
 
 	if ( objectThatWasHit ) {
 
